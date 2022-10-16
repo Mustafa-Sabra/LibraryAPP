75d54bd5d87454e92ed2272f6ac4a87646013f56
Mustafa Sabra <mustafasabra44@gmail.com>
2022-10-16 22:56:49 +0200

    Adding the STD_TYPES file

---
+++ STD_TYPES.h
@@ -0,0 +1,23 @@
+/**********************************************************************************************/
+/***** Name: Mustafa Sabra           **********************************************************/
+/***** Date: 24/9/2022               **********************************************************/
+/***** Component: Standard types     **********************************************************/
+/**********************************************************************************************/
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char 		u8	;
+typedef unsigned short 		u16	;
+typedef unsigned int 		u32	;
+typedef unsigned long 		u64	;
+
+typedef signed char 		s8	;
+typedef signed short 		s16	;
+typedef signed int	 		s32	;
+
+typedef float				f32	;
+typedef double 				f64	;
+typedef long double 		f96	;
+
+#endif
